4ca27243cfac39323310c030ee0368e00e743334
Zackie <102979057+MasterZack69@users.noreply.github.com>
2026-02-24 13:57:25 +0530

    Update cli.rs

--- src/cli.rs
+++ src/cli.rs
@@ -26,7 +26,7 @@ pub async fn run_download(
     let connections = connections.max(1);
 
     let client = reqwest::Client::builder()
-        .user_agent("rdm/0.1.9")
+        .user_agent("rdm")
         .connect_timeout(Duration::from_secs(10))
         .read_timeout(Duration::from_secs(30))
         .build()
