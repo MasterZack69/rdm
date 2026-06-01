2505799cbad776410fd50f654ee233f590f8362e
MasterZack69 <zackdevil2000@gmail.com>
2026-06-01 13:11:53 +0530

    nix flake

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
+  };
+
+  outputs =
+    { nixpkgs, systems, ... }:
+    let
+      eachSystem = nixpkgs.lib.genAttrs (import systems);
+    in
+    {
+      devShells = eachSystem (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              rustup
+            ];
+            shellHook = ''
+              echo "Shell Loaded"
+            '';
+          };
+        }
+      );
+    };
+}
