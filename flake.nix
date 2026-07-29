a8765043d19c9f261dfed646a8b7175af34d5cc4
MasterZack69 <zackdevil2000@gmail.com>
2026-07-29 16:09:54 +0530

    flake.nix: add package output for nix build

--- flake.nix
+++ flake.nix
@@ -10,6 +10,31 @@
       eachSystem = nixpkgs.lib.genAttrs (import systems);
     in
     {
+      packages = eachSystem (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.rustPlatform.buildRustPackage {
+            pname = "rdm";
+            version = "0.2.2";
+            src = ./.;
+
+            cargoLock.lockFile = ./Cargo.lock;
+
+            doCheck = false;
+
+            meta = {
+              description = "High-performance MEGA & HTTP/HTTPS download manager";
+              homepage = "https://github.com/MasterZack69/rdm";
+              license = pkgs.lib.licenses.mit;
+              mainProgram = "rdm";
+            };
+          };
+        }
+      );
+
       devShells = eachSystem (
         system:
         let
